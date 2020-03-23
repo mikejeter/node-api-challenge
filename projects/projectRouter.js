f11ff9ddf660a72f8ce962e4eae8282bb156e2dd
mikejeter <mjeter45@yahoo.com>
2020-03-22 23:22:50 -0400

    Answered questions modified projectRouter.js

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -23,7 +23,7 @@ router.get("/:id", validateId, (req, res) => {
 
   projectModel
     .get(id)
-    .then(projects => res.status(200).json())
+    .then(project => res.status(200).json(project))
     .catch(err =>
       res.status(500).json({ message: "Server was unable to retrieve project", error: err })
     );
