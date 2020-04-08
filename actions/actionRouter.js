5286129b9112ff172c1311572440c7d2d0552487
mikejeter <mjeter45@yahoo.com>
2020-04-07 20:45:02 -0400

    Added the GET method to actionRouter.js

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -5,6 +5,15 @@ const { validateId, validateAction } = require("./actionMiddleware.js");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  actionDb
+    .get()
+    .then(actions => res.status(200).json(actions))
+    .catch(err =>
+      res.status(500).json({ message: "Server was unable to retrieve actions", error: err })
+    );
+});
+
 router.get("/:id", validateId, (req, res) => {
   const { id } = req.params;
 
